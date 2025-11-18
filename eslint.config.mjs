ef56f4897f640e374833d26dbe4d836a5a4b7e33
Siddu Revu <siddurevu001@gmail.com>
2025-11-18 12:58:53 +0000

    error with ci in run lint wt to do Run npm run lint   npm run lint   sh

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,33 @@
-
-import next from 'eslint-config-next';
 import { defineConfig } from 'eslint-define-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-const eslintConfig = defineConfig([
-    {
-        ...next,
-        rules: {
-            ...next.rules,
-            'react/no-unescaped-entities': 'off',
-            '@next/next/no-page-custom-font': 'off',
-        }
-    }
+export default defineConfig([
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@next/next': nextPlugin,
+      react: reactPlugin,
+      'react-hooks': hooksPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  },
 ]);
-
-export default eslintConfig;
