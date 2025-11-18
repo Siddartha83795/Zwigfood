f648ea4f408ff104979483505dc83d7f316f1fca
Siddu Revu <siddurevu001@gmail.com>
2025-11-18 13:25:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6931303873-2959a",
+  "appId": "1:838700804809:web:0fbe05e017987a5acc7b18",
+  "apiKey": "AIzaSyDTl5qNi1z2C9LJ6w_wlc4_ksyg9ruwyC8",
+  "authDomain": "studio-6931303873-2959a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "838700804809"
+};
